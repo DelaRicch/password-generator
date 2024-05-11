2622ffbaf227f57339cba4326c43171f404cefa7
Dela Ricch <delaricch@gmail.com>
2024-05-11 13:40:49 +0000

    feature: add button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
 import { Component, HostBinding } from "@angular/core"
 import { PasswordCopyComponent } from "./components/password-copy/password-copy.component"
+import { PasswordStrengthDisplayComponent } from "./components/password-strength-display/password-strength-display.component"
 
 @Component({
     selector: "app-root",
     standalone: true,
-    imports: [PasswordCopyComponent],
+    imports: [PasswordCopyComponent, PasswordStrengthDisplayComponent],
     template: `
         <section
             class="w-[95vw] sm:w-[80vw] md:w-[540px] flex flex-col gap-4 border-2 border-red-700"
         >
             <h2 class="text-center text-grey font-bold">Generate a password</h2>
             <pg-password-copy></pg-password-copy>
+            <div class="flex flex-col gap-4 p-4 bg-dark">
+                <pg-password-strength-display></pg-password-strength-display>
+                <button
+                    class="transition-all ease-linear duration-200 outline-success w-full h-[65px] border-2 text-black hover:text-success border-transparent hover:border-success
+                   font-bold bg-success hover:bg-transparent group"
+                >
+                    Generate
+                    <span
+                        class="text-2xl group-hover:text-success transition-all ease-linear duration-200"
+                        >→</span
+                    >
+                </button>
+            </div>
         </section>
     `,
 })
--- src/app/components/password-strength-display/password-strength-display.component.ts
+++ src/app/components/password-strength-display/password-strength-display.component.ts
@@ -1,12 +1,21 @@
 import { Component, HostBinding } from "@angular/core"
+import { cn } from "../../utility/functions"
 
 @Component({
     selector: "pg-password-strength-display",
     standalone: true,
     imports: [],
-    template: ``,
+    template: `
+        <span class="uppercase text-grey font-medium">strength</span>
+        <div class="flex items-center gap-4">
+            <span class="capitalize text-ash font-bold text-xl">too weak!</span>
+            <span [class]="cn('w-2.5 h-7 border-[3px]')"></span>
+        </div>
+    `,
 })
 export class PasswordStrengthDisplayComponent {
+    cn = cn
+
     @HostBinding("class") get pgClass() {
         return "flex items-center justify-between px-4 h-[72px] bg-black"
     }
